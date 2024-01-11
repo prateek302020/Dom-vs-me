181513b8a97d5c6239e3b0e7e194610b5d7e71e7
prateek302020 <84118940+prateek302020@users.noreply.github.com>
2024-01-11 13:22:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var dom = document.getElementById("id");
+dom.style.backgroundColor = "yellow";
+dom.style.margin = "20px";
+dom.style.padding = "10px";
+dom.style.fontSize = "18px";
+dom.style.fontWeight = "bold";
+dom.style.height = "200px";
+dom.style.width = "300px";
+dom.style.color = "white";
